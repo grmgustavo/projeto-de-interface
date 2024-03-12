808749c35e9bb40f8868b7222bb103bc2e7374dd
Gustavo Rodrigues Machado <61809841+grmgustavo@users.noreply.github.com>
2024-03-11 21:45:02 -0300

    melhora o código

--- my-chess/script.js
+++ my-chess/script.js
@@ -1,20 +1,15 @@
-document.addEventListener('DOMContentLoaded',
-    function () {
-        const board = document.getElementById('chessboard')
+document.addEventListener('DOMContentLoaded', function () {
+    const board = document.getElementById('chessboard');
+    const numRows = 8;
+    const numCols = 8;
 
-        for (let row = 0; row < 8; row++) {
-            for (let col = 0; col < 8; col++) {
-                let square = document.createElement('div')
+    // Criação dos quadrados do tabuleiro utilizando apenas um loop
+    for (let i = 0; i < numRows * numCols; i++) {
+        const row = Math.floor(i / numCols);
+        const col = i % numCols;
 
-                square.className =
-                    (row + col) % 2 === 0 ?
-                        'chessboard-square white-square' :
-                        'chessboard-square black-square'
-
-                board.appendChild(square)
-            }
-        }
-
-
+        let square = document.createElement('div');
+        square.className = (row + col) % 2 === 0 ? 'chessboard-square white-square' : 'chessboard-square black-square';
+        board.appendChild(square);
     }
-)
+});
